8bb769a9f7fb033c4f8ccb35b7ac1ad614928587
artijoker <59118504+artijoker@users.noreply.github.com>
2023-03-09 21:24:01 +0300

    Update httpHeader.ts

--- BlogAdminClient/src/app/httpHeader.ts
+++ BlogAdminClient/src/app/httpHeader.ts
@@ -3,13 +3,17 @@ import {HttpHeaders} from "@angular/common/http";
 export class HttpHeader {
     public static getAuthorizationHttpHeader(){
         let token = localStorage.getItem("token");
-        return new HttpHeaders().set("Authorization", "Bearer " + token);
+        if (token)
+            return new HttpHeaders().set("Authorization", "Bearer " + token);
+        return new HttpHeaders();
     }
 
     public static getAuthorizationAndContentTypeHttpHeaders(){
         let token = localStorage.getItem("token");
-        return new HttpHeaders().set("Authorization", "Bearer " + token)
-            .set("content-type", "application/json");
+         if (token)
+            return new HttpHeaders().set("Authorization", "Bearer " + token)
+                .set("content-type", "application/json");
+        return HttpHeader.getContentTypeHttpHeaders();
     }
 
     public static getContentTypeHttpHeaders(){
